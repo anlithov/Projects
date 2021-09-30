3d590b156a8ad0069393c05cda5b78a794fd821e
Anlithov <anlithov@gmail.com>
2021-09-30 22:05:32 +0300

    optimization

--- src/web-pages/_components/ComponentsShowdown.js
+++ src/web-pages/_components/ComponentsShowdown.js
@@ -1,58 +1,17 @@
 import React from "react";
+import ShowdownItem from "./components-showdown/ShowdownItem";
 
-export default function ComponentsShowdown() {
-    const shdwncomponents = [
-        {name: 'components-intro'},
-        {name: 'buttons'},
-        {name: 'cards'},
-        {name: 'checkboxes'},
-        {name: 'chips'},
-        {name: 'lists'},
-        {name: 'icons'},
-        {name: 'pagination'},
-        {name: 'radio-buttons'},
-        {name: 'select'},
-        {name: 'text-fields'},
-    ]
+export default function ComponentsShowdown(props) {
+
     return (
         <div className="showdown-components">
+            {props.components.map(shdwncomp => {
+                return ShowdownItem(shdwncomp={shdwncomp})
+            })}
+            <a href="" className={props.components[0].name + ' shdwn-component'}>
+                <span>{props.components[0].name.replace('-',' ')}</span>
 
-            <div className="components-intro shdwm-component active">
-
-            </div>
-            <div className="buttons shdwm-component">
-
-            </div>
-            <div className="cards shdwm-component">
-
-            </div>
-            <div className="checkboxes shdwm-component">
-
-            </div>
-            <div className="chips shdwm-component">
-
-            </div>
-            <div className="lists shdwm-component">
-
-            </div>
-            <div className="headers shdwm-component">
-
-            </div>
-            <div className="icons shdwm-component">
-
-            </div>
-            <div className="pagination shdwm-component">
-
-            </div>
-            <div className="radiobuttons shdwm-component">
-
-            </div>
-            <div className="select shdwm-component">
-
-            </div>
-            <div className="textfields shdwm-component">
-
-            </div>
+            </a>
         </div>
 
     )
--- src/web-pages/components.js
+++ src/web-pages/components.js
@@ -3,10 +3,23 @@ import ComponentsNav from './_components/ComponentsNav'
 import ComponentsShowdown from './_components/ComponentsShowdown'
 
 export default function Components() {
+    const components = [
+        {name: 'components-intro'},
+        {name: 'buttons'},
+        {name: 'cards'},
+        {name: 'checkboxes'},
+        {name: 'chips'},
+        {name: 'lists'},
+        {name: 'icons'},
+        {name: 'pagination'},
+        {name: 'radio-buttons'},
+        {name: 'select'},
+        {name: 'text-fields'},
+    ]
     return (
         <div className="page-components">
-            <ComponentsNav />
-            <ComponentsShowdown />
+            <ComponentsNav components={components}/>
+            <ComponentsShowdown components={components}/>
         </div>
 
     )
