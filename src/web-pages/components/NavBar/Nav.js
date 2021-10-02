0898b1894a2ff908d87af599b38457d042f234a4
Anlithov <anlithov@gmail.com>
2021-10-02 19:00:00 +0300

    Optimization; Tabs on Components

--- src/web-pages/components/NavBar/Nav.js
+++ src/web-pages/components/NavBar/Nav.js
@@ -2,9 +2,6 @@ import React from "react";
 
 export default function Nav({components, selectedTab, handleNavChange}) {
 
-    function handleNavClick(index) {
-        handleNavChange(index)
-    }
 
     return (
 
@@ -17,8 +14,7 @@ export default function Nav({components, selectedTab, handleNavChange}) {
             <div className="nav-list">
                 {components.map( (navitem, index) => {
                     return (
-                        /*<NavItem navitem={navitem} onClick={() => handleNavChange(index)} active={index === selectedTab}/>*/
-                        <a className={navitem.name + ` nav-item ${index === selectedTab ? 'active' : '' }`} onClick={handleNavClick} >
+                        <a className={navitem.name + ` nav-item ${index === selectedTab ? 'active' : '' }`} onClick={() => handleNavChange(index)} >
                             <span>{navitem.name.replace('-',' ')}</span>
                         </a>
                     )
