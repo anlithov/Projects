bd6d31fd3bb33bf297d9d0485dba3bce4cae8c78
Anlithov <anlithov@gmail.com>
2021-10-06 14:37:18 +0300

    improvements

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>npm
+  <React.StrictMode>
       <App />
   </React.StrictMode>,
   document.getElementById('root')
