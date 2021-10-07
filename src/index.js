7d27f8afbaa97d01981820aaf4b05d927538e0f7
Anlithov <anlithov@gmail.com>
2021-10-07 13:21:39 +0300

    Tree reorganization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import WebFont from 'webfontloader';
-
-WebFont.load({
-    google: {
-        families: ['Arimo Web:400,500,600,700', 'sans-serif']
-    }
-});
 
 ReactDOM.render(
   <React.StrictMode>
